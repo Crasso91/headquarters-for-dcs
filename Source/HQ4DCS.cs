b0a67f03e8325701d4ec48ee6bc2b71ad17b180e
AgarCdF <ambroise.garel@gmail.com>
2019-07-24 13:48:48 +0200

    Cleaned up HQ4DCS class

--- Source/HQ4DCS.cs
+++ Source/HQ4DCS.cs
@@ -24,33 +24,12 @@ along with HQ4DCS. If not, see https://www.gnu.org/licenses/
 
 using Headquarters4DCS.Forms;
 using Headquarters4DCS.Library;
-//using Headquarters4DCS.Template;
 using System;
 using System.Reflection;
 using System.Windows.Forms;
 
 namespace Headquarters4DCS
 {
-    /*
-     * HOW DOES HQ4DCS WORK?
-     * 
-     * HQ4DCS has been designed to be as simple as possible. Its basic principle is as follows:
-     * 
-     * 1. Populate the library (Headquarters4DCS.Library.HQLibrary) with data from the .ini files found in the Library directory.
-     *    This data will be used to know what common parameters should be used, such as how far each type of enemy air defense should
-     *    be spawned from the player's original position, but also what options should be offered to the player in the mission template
-     *    menu (theaters, coalitions...).
-     * 2. Let the user create a mission template (Headquarters4DCS.Template.MissionTemplate) from the WinForms interface, or load one from
-     *    a template (.hqt file – basically just a renamed .ini file)
-     * 3. When the user clicks the "generate mission" button, create an instance of the mission generator
-     *    (Headquarters4DCS.Generator.MissionGenerator) which will convert the mission template into an HQMission
-     *    (Headquarters4DCS.Mission.HQMission). The HQMission contains all information about the mission: briefing, unit groups, etc.
-     * 4. When the user decides to export the HQMission to a DCS World .miz file, create an instance of the MIZExporter class
-     *    (Headquarters4DCS.MIZExport.MIZExporter) which will create a .miz file from the HQMission using the Lua scripts
-     *    in the Include directory.
-    */
-
-
     /// <summary>
     /// The main application class.
     /// </summary>
@@ -87,16 +66,6 @@ namespace Headquarters4DCS
         private readonly FormMain Form;
 
         /// <summary>
-        /// The mission template.
-        /// </summary>
-        //public readonly MissionTemplate Template;
-
-        /// <summary>
-        /// Settings and presets loaded from the INI subdirectory.
-        /// </summary>
-        //public readonly HQLibrary Library;
-
-        /// <summary>
         /// Constructor. Creates all required sub-classes and starts the application.
         /// </summary>
         public HQ4DCS()
@@ -118,8 +87,7 @@ namespace Headquarters4DCS
         /// </summary>
         public void Dispose()
         {
-            //if (Template != null) Template.Dispose();
-            //if (Library != null) Library.Dispose();
+            if (HQLibrary.Instance != null) HQLibrary.Instance.Dispose();
         }
     }
 }
