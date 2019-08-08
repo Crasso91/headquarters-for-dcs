0a752e6305523f3fc93e0f2964a42d683c00dfe9
AgarCdF <ambroise.garel@gmail.com>
2019-08-08 22:40:31 +0200

    Added TheaterUpdateType

---
+++ Source/Enums/TheaterUpdateType.cs
@@ -0,0 +1,18 @@
+﻿namespace Headquarters4DCS
+{
+    public enum TheaterUpdateType
+    {
+        /// <summary>
+        /// Update the whole theater. Only when theater has changed.
+        /// </summary>
+        Full,
+        /// <summary>
+        /// Update all locations.
+        /// </summary>
+        AllLocations,
+        /// <summary>
+        /// Update only the selected location.
+        /// </summary>
+        SelectedLocation
+    }
+}
