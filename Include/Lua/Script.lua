0872205f036095b30badb2cb39e7bc7676cd9ea6
AgarCdF <ambroise.garel@gmail.com>
2019-08-08 22:09:15 +0200

    Added function hq.everySecond and hq.eventHandler:onEvent

--- Include/Lua/Script.lua
+++ Include/Lua/Script.lua
@@ -5,6 +5,68 @@
 hq = { }
 
 -- ===============================================
+-- EVENT HANDLER
+-- ===============================================
+
+hq.eventHandler = {}
+function hq.eventHandler:onEvent(event)
+  --------------------------
+  -- Target designation mark
+  --------------------------
+--   if hq.enableF10MapTargetDesignation then
+--     if event.id == world.event.S_EVENT_MARK_ADDED or event.id == world.event.S_EVENT_MARK_REMOVED or event.id == world.event.S_EVENT_MARK_CHANGE then
+--       if event.id == world.event.S_EVENT_MARK_REMOVED then table.insert(hq.designationRemovedMarkersIDX, event.idx) end
+--       hq.updateDesignationMenuItems()
+--     end
+--   end
+
+  -------------------------------------------------------
+  -- Ugly hack to make parked airplanes easier to destroy
+  -- DCS won't mark them as destroyed unless they're
+  -- at 0 HP, even if both wings have been clipped off
+  -------------------------------------------------------
+  -- if event.id == world.event.S_EVENT_HIT then
+  --   if target ~= nil then
+  --     if target:inAir() == false and target:getCoalition() == coalition.side.$ENEMYCOALITION$ then
+  --       if (target:getCategory() == Unit.Category.AIRPLANE) or (target:getCategory() == Unit.Category.HELICOPTER) then
+  --         trigger.action.explosion(target:getPoint(), 100)
+  --       end
+  --     end
+  --   end
+  -- end
+
+  -------------------------------------------------------
+  -- Actions to perform when the first player takes off
+  -------------------------------------------------------
+--   if event.id == world.event.S_EVENT_TAKEOFF then
+--     if event.initiator ~= nil then
+--       if (hq.playerTookOff == false) and (event.initiator:getPlayerName() ~= nil) then
+--         hq.playerTookOff = true
+--         for _,v in ipairs(hq.groupsToActiveOnFirstTakeOff) do
+--           local group = hq.getGroupByID(v)
+--           if group ~= nil then group:activate() end
+--         end
+--         hq.groupsToActiveOnFirstTakeOff = { }
+--       end
+--     end
+--   end
+
+  -- Custom events for the mission
+  $SCRIPTEVENT$
+end
+world.addEventHandler(hq.eventHandler)
+
+-- ===============================================
+-- SCHEDULED FUNCTION, RAN EVERY SECOND
+-- ===============================================
+
+function hq.everySecond(args, time)
+  $SCRIPTTIMER$
+  return time + 1
+end
+timer.scheduleFunction(hq.everySecond, nil, timer.getTime() + 1)
+
+-- ===============================================
 -- MISSION SCRIPT
 -- ===============================================
 
