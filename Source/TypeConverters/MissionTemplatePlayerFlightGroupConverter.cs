854a45d68a690545d0cf89636a961619005e5bd4
AgarCdF <47314805+AgarCdF@users.noreply.github.com>
2019-11-16 10:35:46 +0100

    Added Type Converter for MissionTemplatePlayerFlightGroup array

---
+++ Source/TypeConverters/MissionTemplatePlayerFlightGroupConverter.cs
@@ -0,0 +1,21 @@
+﻿using Headquarters4DCS.Template;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Headquarters4DCS.TypeConverters
+{
+    public sealed class MissionTemplatePlayerFlightGroupConverter : ArrayConverter
+    {
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if (destinationType == typeof(string)) return true;
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            return ((MissionTemplatePlayerFlightGroup[])value).Length.ToString() + " flight group(s)";
+        }
+    }
+}
