a4d63674e3d74342ca4184ac5d83596d4034126f
AgarCdF <47314805+AgarCdF@users.noreply.github.com>
2019-11-22 11:28:21 +0100

    Fixed typo

--- Include/Lua/Script.lua
+++ Include/Lua/Script.lua
@@ -73,11 +73,11 @@ timer.scheduleFunction(hq.everySecond, nil, timer.getTime() + 1)
 -- Create common "other" menu
 hq.f10Menu = { }
 missionCommands.addCommandForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.MissionStatus£", nil, hq.f10MissionStatus)
-hq.f10Menu.FlightPlan=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.FlightPlan£", nil))
+hq.f10Menu.FlightPlan=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.FlightPlan£", nil)
 hq.f10Menu.FlightPlanItems = { }
-hq.f10Menu.Objective=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.Objectives£", nil))
+hq.f10Menu.Objective=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.Objectives£", nil)
 hq.f10Menu.ObjectiveItems = { }
-hq.f10Menu.Support=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.Support£", nil))
+hq.f10Menu.Support=missionCommands.addSubMenuForCoalition(coalition.side.$PLAYERCOALITION$, "£F10Menu.Main.Support£", nil)
 hq.f10Menu.SupportItems = { }
 
 $SCRIPTGLOBAL$
