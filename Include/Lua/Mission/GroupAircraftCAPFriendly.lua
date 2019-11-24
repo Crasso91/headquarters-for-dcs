8f646fa80b393fa19cea9984f6532e662045bf71
AgarCdF <ambroise.garel@gmail.com>
2019-11-24 12:06:23 +0100

    Updated mission lua script

---
+++ Include/Lua/Mission/GroupAircraftCAPFriendly.lua
@@ -0,0 +1,125 @@
+[$INDEX$] =
+{
+    ["modulation"] = 0,
+    ["tasks"] = 
+    {
+    }, -- end of ["tasks"]
+    ["task"] = "CAP",
+    ["uncontrolled"] = false,
+    ["route"] = 
+    {
+        ["points"] = 
+        {
+            [1] = 
+            {
+                ["alt"] = $ALTITUDE$,
+                ["action"] = "Turning Point",
+                ["alt_type"] = "BARO",
+                ["speed"] = $SPEED$,
+                ["task"] = 
+                {
+                    ["id"] = "ComboTask",
+                    ["params"] = 
+                    {
+                        ["tasks"] = 
+                        {
+                            [1] = 
+                            {
+                                ["enabled"] = true,
+                                ["key"] = "CAP",
+                                ["id"] = "EngageTargets",
+                                ["number"] = 1,
+                                ["auto"] = true,
+                                ["params"] = 
+                                {
+                                    ["targetTypes"] = 
+                                    {
+                                        [1] = "Air",
+                                    }, -- end of ["targetTypes"]
+                                    ["priority"] = 0,
+                                }, -- end of ["params"]
+                            }, -- end of [1]
+                            [2] = 
+                            {
+                                ["enabled"] = true,
+                                ["auto"] = true,
+                                ["id"] = "WrappedAction",
+                                ["number"] = 2,
+                                ["params"] = 
+                                {
+                                    ["action"] = 
+                                    {
+                                        ["id"] = "EPLRS",
+                                        ["params"] = 
+                                        {
+                                            ["value"] = true,
+                                            ["groupId"] = 1,
+                                        }, -- end of ["params"]
+                                    }, -- end of ["action"]
+                                }, -- end of ["params"]
+                            }, -- end of [2]
+                        }, -- end of ["tasks"]
+                    }, -- end of ["params"]
+                }, -- end of ["task"]
+                ["type"] = "Turning Point",
+                ["ETA"] = 0,
+                ["ETA_locked"] = true,
+                ["y"] = $Y$,
+                ["x"] = $X$,
+                ["name"] = "IP",
+                ["formation_template"] = "",
+                ["speed_locked"] = true,
+            }, -- end of [1]
+            [2] = 
+            {
+                ["alt"] = $ALTITUDE$,
+                ["action"] = "Turning Point",
+                ["alt_type"] = "BARO",
+                ["speed"] = $SPEED$,
+                ["task"] = 
+                {
+                    ["id"] = "ComboTask",
+                    ["params"] = 
+                    {
+                        ["tasks"] = 
+                        {
+                            [1] = 
+                            {
+                                ["enabled"] = true,
+                                ["auto"] = false,
+                                ["id"] = "Orbit",
+                                ["number"] = 1,
+                                ["params"] = 
+                                {
+                                    ["altitude"] = $ALTITUDE$,
+                                    ["pattern"] = "Circle",
+                                    ["speed"] = $SPEED$,
+                                }, -- end of ["params"]
+                            }, -- end of [1]
+                        }, -- end of ["tasks"]
+                    }, -- end of ["params"]
+                }, -- end of ["task"]
+                ["type"] = "Turning Point",
+                ["ETA"] = 0.0,
+                ["ETA_locked"] = false,
+                ["y"] = $CENTERY$,
+                ["x"] = $CENTERX$,
+                ["name"] = "DP",
+                ["formation_template"] = "",
+                ["speed_locked"] = true,
+            }, -- end of [2]
+        }, -- end of ["points"]
+    }, -- end of ["route"]
+    ["groupId"] = $GROUPID$,
+    ["hidden"] = $HIDDEN$,
+    ["units"] = 
+    {
+		$UNITS$	
+    }, -- end of ["units"]
+    ["y"] = $Y$,
+    ["x"] = $X$,
+    ["name"] = "$NAME$",
+    ["communication"] = true,
+    ["start_time"] = 0,
+    ["frequency"] = 251,
+}, -- end of [1]
--- Include/Lua/Script.lua
+++ Include/Lua/Script.lua
@@ -3,6 +3,63 @@
 -- ===============================================
 
 hq = { }
+hq.constants = { }
+hq.constants.artillerySpread = 150 -- artillery inaccuracy radius is 150m
+hq.constants.defaultTextMessageDuration = 5 -- default duration (in seconds) for "subtitles" of radio messages
+hq.constants.smokeDuration = 300 -- smoke markers last for 5 minutes (300 seconds) in DCS World
+hq.constants.timeForRadioAnswer = { 4, 6 } -- min/max time (in seconds) before a radio message gets an answer. Include the initial message duration (around 3 seconds for most messages)
+
+hq.playerTookOff = false -- becomes true once at least one player took off
+hq.radioMessageScheduledID = nil -- The ID of the "answer" radio message (stored so we can cancel/unschedule the function if another radio transmission is made before the answer is played)
+
+------------------------
+-- Plays a radio message
+------------------------
+function hq.playRadioMessage(message, oggFile, message2, oggFile2, duration, functionToRun, functionParameters)
+  -- unschedule any radio message answer
+  if hq.radioMessageScheduledID ~= nil then timer.removeFunction(hq.radioMessageScheduledID) end
+  hq.radioMessageScheduledID = nil
+
+  oggFile = oggFile or "radio0"
+  oggFile = oggFile:lower()
+  duration = duration or hq.constants.defaultTextMessageDuration
+
+  if message2 ~= nil then
+    trigger.action.outTextForCoalition(coalition.side.$PLAYERCOALITION$, message, hq.constants.defaultTextMessageDuration)
+  else
+    trigger.action.outTextForCoalition(coalition.side.$PLAYERCOALITION$, message, duration)
+  end
+
+  trigger.action.outSoundForCoalition(coalition.side.$PLAYERCOALITION$, "l10n/DEFAULT/"..oggFile..".ogg" )
+
+  if message2 ~= nil then
+    oggFile2 = oggFile2 or "radio0"
+    oggFile2 = oggFile2:lower()
+
+    hq.radioMessageScheduledID = timer.scheduleFunction(
+      hq.playRadioMessageAnswer,
+      { message2, oggFile2, duration, functionToRun, functionParameters },
+      timer.getTime() + math.random(hq.constants.timeForRadioAnswer[1], hq.constants.timeForRadioAnswer[2]))
+  else
+    if (functionToRun ~= nil) then
+      functionToRun(functionParameters)
+    end
+  end
+end
+
+function hq.playRadioMessageAnswer(msgParameters, time)
+  trigger.action.outTextForCoalition(coalition.side.$PLAYERCOALITION$, msgParameters[1], msgParameters[3])
+  trigger.action.outSoundForCoalition(coalition.side.$PLAYERCOALITION$, "l10n/DEFAULT/"..msgParameters[2]..".ogg" )
+
+  -- Run function (if a function was provided)
+  if (msgParameters[4] ~= nil) then
+    msgParameters[4](msgParameters[5])
+  end
+  
+  hq.radioMessageScheduledID = nil
+
+  return nil
+end
 
 -- ===============================================
 -- EVENT HANDLER
@@ -69,6 +126,24 @@ timer.scheduleFunction(hq.everySecond, nil, timer.getTime() + 1)
 -- ===============================================
 -- MISSION SCRIPT
 -- ===============================================
+
+-- Request update on mission status
+function hq.f10MissionStatus()
+  if hq.missionStatus < 0 then -- mission failed
+    hq.playRadioMessage("£RadioMessages/Player.MissionStatus£", "PlayerMissionStatus", "£RadioMessages/Radio.MissionStatusFailed£", "RadioMissionStatusFailed")
+  elseif hq.missionStatus > 0 then -- mission complete
+    hq.playRadioMessage("£RadioMessages/Player.MissionStatus£", "PlayerMissionStatus", "£RadioMessages/Radio.MissionStatusComplete£", "RadioMissionStatusComplete")
+  else  -- mission in progress, show status of each objective
+    local objectiveStatusList = "\n";
+    for i=1,hq.objectiveCount do
+      objectiveStatusList = objectiveStatusList.."\nObjective "..hq.objectiveNames[i].." "
+      if (objectiveStatus[i] > 0) then objectiveStatusList = objectiveStatusList.."[X]"
+      else objectiveStatusList = objectiveStatusList.."[ ]" end
+    end
+
+    hq.playRadioMessage("£RadioMessages/Player.MissionStatus£", "PlayerMissionStatus", "£RadioMessages/Radio.MissionStatusInProgress£"..objectiveStatusList, "RadioMissionStatusInProgress")
+  end
+end
 
 -- Create common "other" menu
 hq.f10Menu = { }
